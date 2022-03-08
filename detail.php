0d6c5fd8422bd3457047488a48c8548cf6c41bde
ismisirclo <101158440+ismisirclo@users.noreply.github.com>
2022-03-08 17:41:55 +0700

    Revert "halo"

--- detail.php
+++ detail.php
@@ -8,7 +8,6 @@
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-<<<<<<< HEAD
         <title>Ini Adalah Detail</title>
     </head>   
     <body>
@@ -49,7 +48,6 @@
         </nav>
         
         <h1>Ini Adalah Detail</h1>
-        <h1>Ini Adalah Detail 2</h1>        
 
         <!-- Optional JavaScript -->
         <!-- jQuery first, then Popper.js, then Bootstrap JS -->
